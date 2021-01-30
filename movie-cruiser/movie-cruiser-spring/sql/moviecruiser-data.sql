cf4ed45c26b89f04cb59b3799d100d3a9914a848
jainutsav <47519014+jainutsav@users.noreply.github.com>
2021-01-30 17:42:51 +0530

    Create moviecruiser-data.sql

---
+++ movie-cruiser/movie-cruiser-spring/sql/moviecruiser-data.sql
@@ -0,0 +1,19 @@
+insert into Movies (movie_title, movie_box_office, movie_active, movie_date_of_launch, movie_genre, movie_teaser) values (
+"Tenet", 7.5, true, "2017-03-15", "Science Fiction", "https://www.imdb.com/video/vi1504821529?playlistId=tt6723592&ref_=tt_ov_vi");
+insert into Movies (movie_title, movie_box_office, movie_active, movie_date_of_launch, movie_genre, movie_teaser) values (
+"The Hobbit", 7.8, true, "2017-12-23", "Adventure", "https://www.imdb.com/video/vi650683417?playlistId=tt0903624&ref_=tt_ov_vi");
+insert into Movies (movie_title, movie_box_office, movie_active, movie_date_of_launch, movie_genre, movie_teaser) values (
+"The Game Plan", 6.1, true, "2017-08-21", "Comedy", "https://www.imdb.com/video/vi2649621529?playlistId=tt0492956&ref_=tt_ov_vi");
+insert into Movies (movie_title, movie_box_office, movie_active, movie_date_of_launch, movie_genre, movie_teaser) values (
+"The Avengers", 8.4, false, "2017-07-02", "Superhero", "https://www.imdb.com/video/vi528070681?playlistId=tt4154756&ref_=tt_ov_vi");
+insert into Movies (movie_title, movie_box_office, movie_active, movie_date_of_launch, movie_genre, movie_teaser) values (
+"Shutter Island", 8.2, true, "2022-11-02", "Thriller", "https://www.imdb.com/video/vi4064281?playlistId=tt1130884&ref_=tt_ov_vi");
+insert into Users (user_first_name, user_last_name) values ("Arya","Stark");
+insert into Users (user_first_name, user_last_name) values ("John","Snow");
+
+insert into Favorites values(1,1);
+insert into Favorites values(1,2);
+insert into Favorites values(1,3);
+insert into Favorites values(2,1);
+insert into Favorites values(2,2);
+insert into Favorites values(2,3);
