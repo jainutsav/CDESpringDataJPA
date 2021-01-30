e7724fb14ab9a9210d1794604164ab856eec54c9
jainutsav <47519014+jainutsav@users.noreply.github.com>
2021-01-30 17:16:02 +0530

    Create truyum-data.sql

---
+++ truyum/truyum-spring/sql/truyum-data.sql
@@ -0,0 +1,19 @@
+insert into MenuItems (menu_item_name, menu_item_price, menu_item_active, menu_item_date_of_launch, menu_item_category, menu_item_free_delivery) values (
+"Sandwich", 99.00, true, "2017-03-15", "Main Course", true);
+insert into MenuItems (menu_item_name, menu_item_price, menu_item_active, menu_item_date_of_launch, menu_item_category, menu_item_free_delivery) values (
+"Burger", 129.00, true, "2017-12-23", "Main Course", false);
+insert into MenuItems (menu_item_name, menu_item_price, menu_item_active, menu_item_date_of_launch, menu_item_category, menu_item_free_delivery) values (
+"Pizza", 149.00, true, "2017-08-21", "Main Course", false);
+insert into MenuItems (menu_item_name, menu_item_price, menu_item_active, menu_item_date_of_launch, menu_item_category,  menu_item_free_delivery) values (
+"French Fries", 57.00, false, "2017-07-02", "Starters", true);
+insert into MenuItems (menu_item_name, menu_item_price, menu_item_active, menu_item_date_of_launch, menu_item_category, menu_item_free_delivery) values (
+"Chocolate Brownie", 32.00, true, "2022-11-02", "Dessert", true);
+insert into Users (user_first_name, user_last_name) values ("William","Turner");
+insert into Users (user_first_name, user_last_name) values ("Elizabeth","Swann");
+
+insert into Cart values(1,1);
+insert into Cart values(1,2);
+insert into Cart values(1,3);
+insert into Cart values(2,1);
+insert into Cart values(2,2);
+insert into Cart values(2,3);
